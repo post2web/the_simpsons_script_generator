388efcc37e2e01c78edde16b5e986916f82645e1
Ivelin Angelov <post2web@gmail.com>
2017-09-20 21:27:29 -0700

    updated

--- tokenizer.py
+++ tokenizer.py
@@ -3,26 +3,28 @@ import re
 
 value_to_token = {
     '.': '[period]',
+    '!': '[exclamation_mark]',
+    '?': '[question_mark]',
     ',': '[comma]',
     '"': '[quotation_mark]',
     "'": '[apostrophe]',
     ':': '[colon]',
     ';': '[semicolon]',
-    '!': '[exclamation_mark]',
-    '?': '[question_mark]',
     '(': '[left_parentheses]',
     ')': '[right_parentheses]',
     '-': '[dash]',
+    '&': '[and]',
+    '#': '[pound]',
     '/': '[fslash]',
     '\\': '[bslash]',
     '\n': '[return]',
     '\t': '[tab]',
-    ' ': '[space]',
+    ' ': '[space]'
 }
 
 token_to_value = dict(zip(value_to_token.values(), value_to_token.keys()))
 
-def text_to_tokens(text):
+def text_to_tokens(text, vocab=None):
     text = text.replace('[', '(').replace(']', ')')
     for key, token in value_to_token.items():
         text = text.replace(key, token)
@@ -37,7 +39,13 @@ def text_to_tokens(text):
         if token[0].isupper():
             new_tokens.append('[capital]')
         new_tokens.append(token.lower())
-
+    if vocab is not None:
+        tokens = []
+        for token in new_tokens:
+            if token not in vocab:
+                token = 'not_in_vocab'
+            tokens.append(token)
+        new_tokens = tokens
     return new_tokens
 
 def tokens_to_text(sentence_tokens):
